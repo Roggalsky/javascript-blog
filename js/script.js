8d6c8ba5544b45673d13906a90be7c554a97953d
Dawid Rogalski <roggalsky@gmail.com>
2023-11-09 17:58:23 +0100

    done

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const titleClickHandler = function(event){
     const clickedElement = this;
     console.log('Link was clicked!');
-
+    event.preventDefault();
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -16,13 +16,15 @@ const titleClickHandler = function(event){
     /* [IN PROGRESS] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
-
     /* [DONE] remove class 'active' from all articles */
-
+    const articleSelector = ('href')
+    getAttribute.clickedElement('href')
+    console.log(articleSelector)
     /* get 'href' attribute from the clicked link */
-
+    const targetArticle = document.querySelector('href')
+    console.log(targetArticle)
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    targetArticle.classList.add('active')
     /* add class 'active' to the correct article */
   }
   const links = document.querySelectorAll('.titles a');
