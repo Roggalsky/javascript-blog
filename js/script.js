8c56c168a61de06d0d1effee62fc4f3e57ad73c4
Dawid Rogalski <roggalsky@gmail.com>
2023-11-16 15:59:15 +0100

    fixed const targetArticle

--- js/script.js
+++ js/script.js
@@ -10,43 +10,39 @@ const titleClickHandler = function(event){
   console.log('Link was clicked!');
 
 
- /* [DONE] remove class 'active' from all article links  */
-
- const activeLinks = document.querySelectorAll('.titles a.active');
+/* [DONE] remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     (console.log ('remove active class'))
 }
 
-  /*[IN PROGRESS] add class 'active' to the clicked link */
-  clickedElement.classList.add('active')
-  console.log('clickedElement:', clickedElement);
+/*[IN PROGRESS] add class 'active' to the clicked link */
+clickedElement.classList.add('active')
+console.log('clickedElement:', clickedElement);
 /**
  console.log('clickedElement (with plus): ' + clickedElement);.(przy takim zapisie konsola wyrzuca nam link https na stronę artykułu)
  a tak kieruje nas do liku href.
  */
 
-
-
-  /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active')/*czemu (.posts article.active)i skąd to wziąć?) */
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-      consol.log('remove active from Article') /**co z nim jest nie tak? */
+ /* [DONE] remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.posts article.active')/*czemu (.posts article.active)i skąd to wziąć?) */
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    console.log('remove active from Article') /**co z nim jest nie tak? */
     }
 
-  /* get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute('href')
-  console.log('get href')
+/* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+    console.log('get href')
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-    const targetArticle = document.querySelector ('articleSelector')
+/* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector (articleSelector)
 
-    /* add class 'active' to the correct article */
-    targetArticle.classList.add('active')
+/* add class 'active' to the correct article */
+targetArticle.classList.add('active')
 
 }
-
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
