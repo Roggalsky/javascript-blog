0e63cd76a38974df137cee77f85558462311a629
Dawid Rogalski <roggalsky@gmail.com>
2023-11-16 15:30:43 +0100

    add find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -4,20 +4,47 @@
   console.log('links:', links);
 });
 */
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
 
- /* remove class 'active' from all article links  */
 
-  /* add class 'active' to the clicked link */
+ /* [DONE] remove class 'active' from all article links  */
 
-  /* remove class 'active' from all articles */
+ const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+    (console.log ('remove active class'))
+}
+
+  /*[IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active')
+  console.log('clickedElement:', clickedElement);
+/**
+ console.log('clickedElement (with plus): ' + clickedElement);.(przy takim zapisie konsola wyrzuca nam link https na stronę artykułu)
+ a tak kieruje nas do liku href.
+ */
+
+
+
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active')/*czemu (.posts article.active)i skąd to wziąć?) */
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+      consol.log('remove active from Article') /**co z nim jest nie tak? */
+    }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href')
+  console.log('get href')
 
   /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector ('articleSelector')
 
-  /* add class 'active' to the correct article */
+    /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
+
 }
 
 const links = document.querySelectorAll('.titles a');
