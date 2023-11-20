7b6035afdbab0913ed34626dd93bfe03e7d3f14a
Dawid Rogalski <roggalsky@gmail.com>
2023-11-20 14:54:47 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -30,36 +30,48 @@ const targetArticle = document.querySelector (articleSelector)
 /* add class 'active' to the correct article */
 targetArticle.classList.add('active')
 }
-const links = document.querySelectorAll('.titles a')
+const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
-{
+
   const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-  function generateTitleLinks() {
+function generateTitleLinks() {
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
-  /* for each article */
-const articles = document.querySelectorAll(optArticleSelector);
-for(let Article of articles){
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for (let article of articles) {
     /* get the article id */
-const articleId = article.getAttribute('id');
+    const articleId = article.getAttribute('id');
+
     /* find the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    /* get the title from the title element */
 
     /* create HTML of the link */
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-    /* insert link into titleList */
-    titleList.innerHTML = titleList.innerHTML + linkHTML;
-    console.log('generateTitleLinks executed!');
-    }
- }
-generateTitleLinks();
+
+    /* insert link into html variable */
+    html += linkHTML;
+  }
+
+  /* insert all links into titleList */
+  titleList.innerHTML = html;
+
+  /* add click event to each generated link */
+  const links = document.querySelectorAll('.titles a');
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
